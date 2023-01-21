8f08285fb31280fbb8f1425ef86e58256a97a75f
Daftpool25 <christopher.acosta2626@gmail.com>
2023-01-21 06:16:44 +0100

    deploy

--- models/sqlModel.js
+++ models/sqlModel.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
+const { PASSWORD, HOST_DB, DATABASE,USER } = require("../utils/config");
 
 const connection =mysql.createConnection({
-    host:"localhost",
-    database:"herbatica",
-    user:"root",
-    password:"Damian26"
+    host:HOST_DB,
+    database:DATABASE,
+    user:USER,
+    password:PASSWORD
 });
 
 module.exports=connection;
--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,14 @@
 module.exports={
     PORT:  process.env.PORT || 2000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    NODE_ENV: process.env.NODE_ENV ,
     HOST: process.env.HOST || '127.0.0.1',
-    SECRET: process.env.SECRET ||"herbaticaherbatica",
-    YOUR_DOMAIN : 'http://localhost:3000/carrito'
+    SECRET: process.env.SECRET ,
+    YOUR_DOMAIN : process.env.YOUR_DOMAIN,
+    //DATABASE
+    HOST_DB: process.env.HOST,
+    DATABASE: process.env.DATABASE,
+    USER: process.env.USER,
+    PASSWORD: process.env.PASSWORD
+    //
 
 }
