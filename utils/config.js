c40a02f1317e92a73979e0f7a4393d61ccf3edcf
Daftpool25 <christopher.acosta2626@gmail.com>
2023-01-21 07:18:22 +0100

    deploy 4

--- utils/config.js
+++ utils/config.js
@@ -1,14 +1,13 @@
 module.exports={
     PORT:  process.env.PORT || 2000,
     NODE_ENV: process.env.NODE_ENV ,
-    HOST: process.env.HOST || '127.0.0.1',
     SECRET: process.env.SECRET ,
     YOUR_DOMAIN : process.env.YOUR_DOMAIN,
     //DATABASE
-    HOST_DB: process.env.HOST ,
-    DATABASE: process.env.DATABASE ,
-    USER: process.env.USER ,
-    PASSWORD: process.env.PASSWORD 
+    HOST_DB: process.env.HOST || "sql10.freesqldatabase.com",
+    DATABASE: process.env.DATABASE || "sql10592190" ,
+    USER: process.env.USER || "sql10592190",
+    PASSWORD: process.env.PASSWORD || "4Q6dBQl3ng" 
     //
 
 }
